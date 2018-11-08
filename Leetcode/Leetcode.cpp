245af4ad40d0cb050d5dcf4e9f0d3080927b52e3
feihuomars <feihuomars@outlook.com>
2018-11-08 22:54:30 +0800

    添加项目文件。

---
+++ Leetcode/Leetcode.cpp
@@ -0,0 +1,24 @@
+// Leetcode.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+using namespace std;
+
+bool Find(int target, vector<vector<int> > array) {
+	for (int i = 0; i < array.size(); i++) {
+		for (int j = 0; j < array[i].size(); j++) {
+			cout << array[i][j] << endl;
+		}
+	}
+	return false;
+}
+
+int main()
+{
+	vector<vector<int>> array = { {1, 2, 3}, {4, 5, 6} };
+	Find(2, array);
+	return 0;
+}
+
+
