99e97d5cc4a241e1c24d23952631ec45b09b079e
xiaohan <feihuomars@outlook.com>
2018-11-09 18:25:59 +0800

    新算法

--- Leetcode/Leetcode.cpp
+++ Leetcode/Leetcode.cpp
@@ -5,22 +5,59 @@
 
 using namespace std;
 
-bool Find(int target, vector<vector<int> > array) {
+//bool Find(int target, vector<vector<int> > array) {
+//
+//	int start = 0, end = 0;
+//	int size = array.size();
+//
+//	if ( array.size() == 0 || target > array[size - 1][size - 1] || target < array[0][0]) {
+//		return false;
+//	}
+//
+//	for (int i = 0; i < size; i++) {
+//		if (target <= array[i][i]) {
+//			start = i - 1;
+//			end = i;
+//			break;
+//		}
+//	}
+//
+//	for (int i = start; i < size; i++) {
+//		if (target == array[i][start])
+//			return true;
+//	}
+//
+//	for (int i = 0; i <= end; i++) {
+//		if (target == array[i][end])
+//			return true;
+//	}
+//	return false;
+//}
 
-	int offsetX, offsetY;
-
-	for (int i = 0; i < array.size(); i++) {
-		for (int j = 0; j < array[i].size(); j++) {
-			cout << array[i][j] << endl;
+bool Find(int target, vector<vector<int>> array) {
+	int size = array.size();
+	int i = size - 1, j = 0;
+	int pointer = array[size - 1][0];
+	while (i > 0 && j < size) {
+		if (target > pointer) {
+			j++;
+			pointer = array[i][j];
+		} else if (target < pointer) {
+			i--;
+			pointer = array[i][j];
 		}
+		
+		if (target == pointer)
+			return true;
 	}
 	return false;
 }
 
 int main()
 {
-	vector<vector<int>> array = { {1, 2, 3}, {4, 5, 6} };
-	Find(2, array);
+	vector<vector<int>> array = { {1, 17, 29, 57}, {6, 19, 33, 62}, {33, 37, 39, 67}, {48, 52, 58, 69}};
+	//vector<vector<int>> array = { {1} };
+	cout << Find(58, array) << endl;
 	return 0;
 }
 
