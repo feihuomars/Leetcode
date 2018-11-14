e700931bf1aee49c00361a8bffe9024f4e84b12d
xiaohan <feihuomars@outlook.com>
2018-11-14 18:14:15 +0800

    通过字符串替换

--- Leetcode/Leetcode.cpp
+++ Leetcode/Leetcode.cpp
@@ -8,9 +8,20 @@ using namespace std;
 void replaceSpace(char *str, int length) {
 	char *p = str;
 	char *q = str + length;
+	char *end = str + length;
 	while (*p != '\0') {
+		
 		if ((int)*p == 32) {
+			q = end;
+			while (p != q) {
+				*(q + 2) = *q;
+				q--;
+			}
+			end += 2;
 			*p = '%';
+			*(p + 1) = '2';
+			*(p + 2) = '0';
+			p++;
 		}
 		p++;
 	}
@@ -20,15 +31,15 @@ int main()
 {
 	char str[100] = "iii jjj kkk ";
 	
-	char *p = str;
-	cout << (int)*(p + 12);
+	//char *p = str;
+	//cout << (int)*(p + 11);
 
-	/*replaceSpace(str, 12);
+	replaceSpace(str, 12);
 	char *p = str;
 	while (*p != '\0') {
 		cout << *p << endl;
 		p++;
-	}*/
+	}
 	
 	return 0;
 }
