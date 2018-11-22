80cf30381b82da8bd350cda5c5e933b193f46b30
xiaohan <feihuomars@outlook.com>
2018-11-22 18:12:32 +0800

    二叉树

--- Leetcode/Leetcode.cpp
+++ Leetcode/Leetcode.cpp
@@ -13,13 +13,35 @@ struct TreeNode {
 };
 
 TreeNode* reConstructBinaryTree(vector<int> pre, vector<int> vin) {
-	TreeNode *tree;
+	vector<int> leftTree, rightTree;
+	if (pre.size() == 0 || vin.size() == 0)
+		return NULL;
+	TreeNode *treeNode = new TreeNode(pre[0]);
+	
+	for (vector<int>::iterator p = vin.begin(); p != vin.end(); p++) {
+		if (*p == pre.at(0)) {
+
+			leftTree.assign(vin.begin(), p);
+			rightTree.assign(p + 1, vin.end());
+			pre.erase(pre.begin());
+		}
+	}
+	
+	treeNode->left = reConstructBinaryTree(pre, leftTree);
+	treeNode->right = reConstructBinaryTree(pre, rightTree);
+	return treeNode;
 }
 
 int main()
 {
 	TreeNode *bTree;
-
+	
+	
+	//vector<int> pre = { 1, 2, 4, 5, 3, 6, 7 };
+	//vector<int> vin = { 4, 2, 5, 1, 3, 7, 6 };
+	vector<int> pre = { 1, 2, 3 };
+	vector<int> vin = { 2, 1, 3 };
+	bTree = reConstructBinaryTree(pre, vin);
 	return 0;
 }
 
