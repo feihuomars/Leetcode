b781f2ec494184abe951784cff91d3bf322e7098
xiaohan <feihuomars@outlook.com>
2018-11-26 18:23:57 +0800

    jumpFloor2

--- Leetcode/Leetcode.cpp
+++ Leetcode/Leetcode.cpp
@@ -5,25 +5,29 @@
 
 using namespace std;
 
-class Solution
-{
-public:
-	void push(int node) {
-
+int jumpFloorII(int number) {
+	if (number <= 0)
+		return 0;
+	if (number == 1)
+		return 1;
+	if (number == 2)
+		return 2;
+	vector<int> num;
+	num.push_back(1);
+	num.push_back(1);
+	int sum = 0;
+	for (int i = 2; i <= number; i++) {
+		for (int j = 0; j < i; j++) {
+			sum = sum + num.at(j);
+		}
+		num.push_back(sum);
 	}
-
-	int pop() {
-
-	}
-
-private:
-	stack<int> stack1;
-	stack<int> stack2;
-};
+	return num[number];
+}
 
 int main()
 {
-	
+	jumpFloorII(4);
 	return 0;
 }
 
