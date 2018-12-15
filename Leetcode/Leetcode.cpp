7e4f3e8c604d781fa1df1c3911f293ab2483ad78
xiaohan <feihuomars@outlook.com>
2018-12-15 20:12:57 +0800

    层次遍历

--- Leetcode/Leetcode.cpp
+++ Leetcode/Leetcode.cpp
@@ -5,30 +5,54 @@
 
 using namespace std;
 
-bool IsPopOrder(vector<int> pushV, vector<int> popV) {
-	if (pushV.size() == 0)
-		return false;
-	int size = pushV.size(), j = 0;
-	for (int i = size - 1; i > 0; i--) {
-		if (pushV[i] == popV[size - i - 1]) {
-			j = i;
-			break;
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};
+
+vector<int> PrintFromTopToBottom(TreeNode* root) {
+	vector<int> answer;
+	queue<TreeNode*> q;
+	q.push(root);
+	TreeNode* p;
+	while (!q.empty()) {
+		p = q.front();
+		q.pop();
+		answer.push_back(p->val);
+		if (p->left != NULL) {
+			q.push(p->left);
 		}
+		if (p->right != NULL) {
+			q.push(p->right);
+		}
+		
+		
 	}
-	if (j == 0)
-		return true;
-	for (j; j > 0; j--) {
-		if (pushV[j] != popV[size - j - 1])
-			return false;
-	}
-	return true;
+	return answer;
 }
 
+TreeNode *createTree(vector<int> num, int &n) {
+	if (num[n] == 0)
+		return NULL;
+	TreeNode *root = new TreeNode(num[n]);
+	n++;
+	root->left = createTree(num, n);
+	n++;
+	root->right = createTree(num, n);
+	return root;
+}
 
 int main()
 {
-	vector<int> pushV = { 1, 2, 3, 4, 5 }, popV = {4, 3, 5, 1, 2};
-	bool i = IsPopOrder(pushV, popV);
+	vector<int> num = { 1, 2, 0, 0, 3, 0, 4, 0, 0 };
+	int n = 0;
+	TreeNode *root = createTree(num, n);
+	
+	
 	return 0;
 }
 
